71dbb98e33049578c54cee8894849653c7a8c8ed
40392145 <40392145@ads.qub.ac.uk>
2023-05-24 17:31:36 +0100

    Guessing game

---
+++ guessing_game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+
+int main(){
+    int num;
+    int guess;
+
+    srand(time(NULL));
+    num = (rand()%10)+1;
+    std::cout<<"+++++ Guessing Game +++++\n\n";
+    int tries =4;
+    do {
+        tries--;
+        std::cout<<"Guess the number between 1-10, you have "<<tries<<" tries left: ";
+        std::cin>>guess;
+    }while(num!=guess && tries>0);
+    
+    num==guess ? std::cout<<"Correct you did it in "<<4-tries<<" tries\n" : std::cout<<"You lost\n";
+    
+    std::cout<<"Press enter to continue . . .";
+    getch();
+    return 0;
+    
+
+
+
+
+}
