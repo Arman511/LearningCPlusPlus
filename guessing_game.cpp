a5c16be72a16b9e98a0eeec34dc6c72b72f78564
40392145 <40392145@ads.qub.ac.uk>
2023-05-24 18:43:25 +0100

    Updated the guessing game to be better

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 #include <conio.h>
 
-int main(){
+int main()
+{
     int num;
     int guess;
 
     srand(time(NULL));
-    num = (rand()%10)+1;
-    std::cout<<"+++++ Guessing Game +++++\n\n";
-    int tries =4;
-    do {
-        tries--;
-        std::cout<<"Guess the number between 1-10, you have "<<tries<<" tries left: ";
-        std::cin>>guess;
-    }while(num!=guess && tries>1);
-    
-    num==guess ? std::cout<<"Correct you did it in "<<4-tries<<" tries\n" : std::cout<<"You lost\n";
-    
-    std::cout<<"Press enter to continue . . .";
+    num = (rand() % 100) + 1;
+    std::cout << "\n\n+++++ Guessing Game +++++\n\n";
+    int tries = 0;
+    do
+    {
+        tries++;
+        std::cout << "Guess the number between 1-100: ";
+        std::cin >> guess;
+
+        if (num > guess)
+        {
+            std::cout << "Too Low\n";
+        }
+        else if (num < guess)
+        {
+            std::cout << "Too High\n";
+        }
+    } while (num != guess);
+
+    std::cout << "Correct you did it in " << tries<<" tries";
+    std::cout << "\n\n++++++++++++++++++++++++++\n\n";
+    std::cout << "Press enter to continue . . .";
     getch();
     return 0;
-    
-
-
-
-
 }
